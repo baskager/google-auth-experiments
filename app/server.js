54534cbd2705e827eff59d077cc5391ab03320ac
Bas Kager <baskager@hotmail.com>
2018-07-19 00:50:05 +0200

    Google sign-in: Added backend processing of sign-in data

--- app/server.js
+++ app/server.js
@@ -1,15 +1,20 @@
 const express = require("express"),
   app = express(),
+  bodyParser = require("body-parser"),
   http = require("http").Server(app),
   debug = require("debug")("auth-server"),
   exphbs = require("express-handlebars"),
   fs = require("fs"),
   config = require("./config"),
-  port = config.port || 3000;
+  port = config.port || 3000,
+  { OAuth2Client } = require("google-auth-library");
 
 const handlebars = exphbs.create({
   defaultLayout: "main"
 });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
@@ -20,6 +25,7 @@ app.use(express.static("public"));
 fs.readFile("credentials/google.json", "utf8", function(err, data) {
   if (err) throw err;
   const credentials = JSON.parse(data);
+  client = new OAuth2Client(credentials.clientId);
 
   app.get("/", function(req, res) {
     res.render("home", {
@@ -28,12 +34,25 @@ fs.readFile("credentials/google.json", "utf8", function(err, data) {
     });
   });
 
-  app.get("/sign-in/google", function(req, res) {
+  app.post("/sign-in/google", function(req, res) {
     let accessToken = req.body.accessToken;
 
-    res.render("secured", {
-      //   account: payload.getUser()
-    });
+    async function verify() {
+      const ticket = await client.verifyIdToken({
+        idToken: accessToken,
+        audience: credentials.clientId // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+      });
+      const payload = ticket.getPayload();
+      //   const userid = payload["sub"];
+      debug(payload);
+      // Send user data back to the client
+      res.send(payload);
+      // If request specified a G Suite domain:
+      //const domain = payload['hd'];
+    }
+    verify().catch(console.error);
   });
 
   app.get("/sign-in/firebase", function(req, res) {
